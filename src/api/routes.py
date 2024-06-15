6b9cc023e31532305c797cd9e6b8ac18953b2439
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-06-15 01:11:08 +0000

    started project in class

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,45 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# /token POST
+# this route is for thwn the user ALREADY excists and needs an access token
+#create a user query with a conditional to see if tyhe user exists, or return None
+@api.route('/token', methods=['POST'])
+def generate_token():
+    # Receiving the request and converting the body of the request into json format 
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    #query the User table to check if the user exists 
+    user = User.query.filter_by(email = email, password = password).first()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    if user is None: 
+        response = {
+            "msg": "Email or Password does not match."
+        }
+        return jsonify(response), 401 
+    
+    access_token = create_access_token(identity=user.id)
+    response = {
+        "access_token": access_token, 
+        "user_id": user.id, 
+        "msg": f"Welcome {user.email}! This worked!" 
     }
+    return jsonify(response), 200 
 
-    return jsonify(response_body), 200
+# create a route for /signup that will add the user's email and password to the database 
+# POST
+#test on postman
+
+#create a protected route for /invoices that will retrieve and return the users' invoices in json format
+#GET 
+# test on postman 
